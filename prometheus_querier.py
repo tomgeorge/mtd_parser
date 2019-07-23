66905939d7d59572f644e2c54a1be4b3c1025e26
Tom George <tg82490@gmail.com>
2019-07-23 12:11:48 -0500

    Update git repo and query correct URL

--- prometheus_querier.py
+++ prometheus_querier.py
@@ -27,8 +27,9 @@ def query_prometheus():
 
 
     # Openshift-monitoring -> Stateful Sets -> Prometheus > copy external link + /api/v1/query
-    url = os.environ['PROMETHEUS_SVC_URL']
+    host = os.environ['PROMETHEUS_SVC_URL']
     port = os.environ['PROMETHEUS_SVC_PORT']
+    url = 'http://' + host + ':' + port
     #url = 'https://prometheus-k8s-openshift-monitoring.apps.toronto-5773.openshiftworkshop.com/api/v1/query'
 
     header = {'Authorization': bearer_token}
